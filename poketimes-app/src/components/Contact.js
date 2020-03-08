8e2a2ef8d599d8e2ed13160f4fcf188bd4ea302e
AxlRicci <alexbricci@gmail.com>
2020-03-08 17:57:02 -0400

    Created Home, About and Contact components and appropriate routes

---
+++ poketimes-app/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Architecto enim, sit optio necessitatibus velit sunt! 
+                Sapiente libero alias dolorum animi repudiandae delectus 
+                dicta quisquam itaque sit deserunt! Molestias, a non.</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ poketimes-app/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Architecto enim, sit optio necessitatibus velit sunt! 
+                Sapiente libero alias dolorum animi repudiandae delectus 
+                dicta quisquam itaque sit deserunt! Molestias, a non.</p>
+        </div>
+    )
+}
+
+export default Home;
